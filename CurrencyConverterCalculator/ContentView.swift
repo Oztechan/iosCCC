59e31b8223e776a47e6e0e226fcce31644429e63
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2019-08-04 15:59:51 +0200

    Initial Commit

---
+++ CurrencyConverterCalculator/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  CurrencyConverterCalculator
+//
+//  Created by Mustafa Ozhan on 04/08/2019.
+//  Copyright © 2019 Mustafa Ozhan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
